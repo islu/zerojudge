578d28f630a5a179d56daae429042b7d79082af5
islu <38793955+islu@users.noreply.github.com>
2023-05-28 21:54:17 +0800

    feat(leetcode): add sql 50

---
+++ leetcode/SQL 50/610.sql
@@ -0,0 +1,54 @@
+/*
+610. Triangle Judgement
+
+SQL Schema:
+
+Create table If Not Exists Triangle (x int, y int, z int)
+Truncate table Triangle
+insert into Triangle (x, y, z) values ('13', '15', '30')
+insert into Triangle (x, y, z) values ('10', '20', '15')
+
+
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+(x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+
+
+Write an SQL query to report for every three line segments whether they can form a triangle.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+
+Example 1:
+
+Input:
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+Output:
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+*/
+
+select x, y, z, case
+    when x + y > z and x + z > y and y + z > x
+    then 'Yes'  else 'No' end as triangle
+from triangle
